bd5dbd1357d1450dbbebb75aa7f8fd98667a74aa
v10acdict <v10acdict@gmail.com>
2019-04-01 16:10:22 +0900

    Arround

--- src/main/java/com/typing/login/aspect/LogAspect.java
+++ src/main/java/com/typing/login/aspect/LogAspect.java
@@ -1,7 +1,9 @@
 package com.typing.login.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -10,13 +12,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class LogAspect {
 
-    @Before("execution(* *..*.*Controller.*(..))")
-    public void startLog(JoinPoint jp) {
+    @Around("execution(* *..*.*Controller.*(..))")
+    public Object startLog(ProceedingJoinPoint jp) throws Throwable {
         System.out.println("メソッド開始:" + jp.getSignature());
+        try {
+            Object result = jp.proceed();
+            System.out.println("メソッド終了:" + jp.getSignature());
+            return result;
+        } catch (Exception e) {
+            System.out.println("メソッド異常終了:" + jp.getSignature());
+            e.printStackTrace();
+            throw e;
+        }
     }
 
-    @After("execution(* *..*.*Controller.*(..))")
-    public void endLog(JoinPoint jp) {
-        System.out.println("メソッド終了:" + jp.getSignature());
-    }
 }
