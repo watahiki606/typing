ba5ba8e86474b2d0016d3d23f51526e420eecd9e
v10acdict <v10acdict@gmail.com>
2019-04-01 16:02:38 +0900

    AOP

--- src/main/java/com/typing/login/aspect/LogAspect.java
+++ src/main/java/com/typing/login/aspect/LogAspect.java
@@ -10,12 +10,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class LogAspect {
 
-    @Before("execution(* com.typing.login.controller.LoginController.getLogin(..))")
+    @Before("execution(* *..*.*Controller.*(..))")
     public void startLog(JoinPoint jp) {
         System.out.println("メソッド開始:" + jp.getSignature());
     }
 
-    @After("execution(* com.typing.login.controller.LoginController.getLogin(..))")
+    @After("execution(* *..*.*Controller.*(..))")
     public void endLog(JoinPoint jp) {
         System.out.println("メソッド終了:" + jp.getSignature());
     }
