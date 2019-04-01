c083ead0165f5b98f2e15d024b3d06290fa70804
v10acdict <v10acdict@gmail.com>
2019-04-01 16:56:18 +0900

    message

--- src/main/java/com/typing/login/aspect/LogAspect.java
+++ src/main/java/com/typing/login/aspect/LogAspect.java
@@ -14,13 +14,13 @@ public class LogAspect {
 
     @Around("execution(* *..*.*Controller.*(..))")
     public Object startLog(ProceedingJoinPoint jp) throws Throwable {
-        System.out.println("メソッド開始:" + jp.getSignature());
+        System.out.println("[method start]:" + jp.getSignature());
         try {
             Object result = jp.proceed();
-            System.out.println("メソッド終了:" + jp.getSignature());
+            System.out.println("[method end]:" + jp.getSignature());
             return result;
         } catch (Exception e) {
-            System.out.println("メソッド異常終了:" + jp.getSignature());
+            System.out.println("[method err]:" + jp.getSignature());
             e.printStackTrace();
             throw e;
         }
